9a879321c09cd4b48bacc036297744025ed1b9e5
rh0mbus <wlegault_@hotmail.com>
2023-10-19 15:43:47 -0400

    updating test scene

--- scenes/main_level.gd
+++ scenes/main_level.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@export var sound_clip : AudioStream
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +9,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$BGM_Player.loop
 	pass
